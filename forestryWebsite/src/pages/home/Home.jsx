f2d9be6d199961f811bd919da6f435ac53660106
MattBeau22 <beaulieum22@mymacewan.ca>
2026-03-19 19:58:09 -0600

    adding themes and commenting out parts of App.css and index.css

--- forestryWebsite/src/pages/home/Home.jsx
+++ forestryWebsite/src/pages/home/Home.jsx
@@ -1,9 +1,15 @@
-import * as S from "./Home.style";
+import "./Home.style.css";
 
 export function Home() {
   return (
     <div>
       <h1>Welcome to the Forestry Website!</h1>
+      {/* <div className="map">
+        <h2>Map</h2>
+      </div>
+      <div className="graph">
+        <h2>Graph</h2>
+      </div> */}
     </div>
   );
 }
