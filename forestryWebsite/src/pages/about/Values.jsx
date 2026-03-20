42f733356da9b81aecb32fdccbeeb90a0ece7ce2
zhirep <pelayoz@mymacewan.ca>
2026-03-20 17:20:26 -0600

    about page

---
+++ forestryWebsite/src/pages/about/Values.jsx
@@ -0,0 +1,38 @@
+export default function Values() {
+  return (
+    <div class="cardContainer">
+      <h1>Our Values</h1>
+
+      <table>
+        <tr>
+          <th><div class="tableContainer"><h2>Accuracy</h2>
+            <p>
+              Provide accurate and reliable wildfire data to help users make
+              informed decisions.
+            </p></div></th>
+
+          <th><div class="tableContainer"><h2>Innovative</h2>
+            <p>
+              We use modern web technologies to improve how wildfire information
+              is delivered, visualized, and interpreted.
+            </p></div></th>
+        </tr>
+
+        <tr>
+          <th><div class="tableContainer"><h2>Responsibility</h2>
+            <p>
+              We are commited to providing information to support safe and responsible
+              environmental practices.
+            </p></div></th>
+
+          <th><div class="tableContainer"><h2>Community Driven</h2>
+            <p>
+              Work with the community as we believe it is everyone's duty to
+              ensure that our forests are well maintained and protected.
+            </p></div></th>
+        </tr>
+
+      </table>
+    </div>
+  );
+}
