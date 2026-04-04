a2add343d6e989ac69f67be56ec5b4b4ef0c26e3
MattBeau22 <beaulieum22@mymacewan.ca>
2026-04-03 21:06:34 -0600

    adding map on home screen

--- forestryWebsite/src/components/HomePage/MapContainer.jsx
+++ forestryWebsite/src/components/HomePage/MapContainer.jsx
@@ -1,10 +1,37 @@
 import "./MapContainer.style.css";
 import mapImage from "../../assets/map_placeholder.jpg";
+import { useEffect } from "react";
+
+import Map from "https://js.arcgis.com/4.25/@arcgis/core/Map.js";
+import MapView from "https://js.arcgis.com/4.25/@arcgis/core/views/MapView.js";
 
 export function MapContainer() {
+  // useEffect(() => {
+  //   const map = new Map({
+  //     basemap: "topo-vector",
+  //   });
+  // }, []);
+  useEffect(() => {
+    const map = new Map({
+      basemap: "topo-vector",
+    });
+
+    let view = new MapView({
+      map: map,
+      container: "MapApp",
+    });
+
+    view.center = [-113.4937, 53.5462];
+    view.zoom = 12;
+  }, []);
+
   return (
     <div className="map-wrapper">
-      <img className="map" src={mapImage} alt="Map" />
+      <div
+        id="MapApp"
+        className="map-container"
+        style={{ height: "120vh" }}
+      ></div>
     </div>
   );
 }
