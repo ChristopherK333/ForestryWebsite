1b4f7e6bb300c154ee2bc5fd575a41838d0c5992
zhirep <pelayoz@mymacewan.ca>
2026-04-06 11:42:18 -0600

    contact form

--- client/src/pages/contact/ContactForm.jsx
+++ client/src/pages/contact/ContactForm.jsx
@@ -1,4 +1,34 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function ContactForm() {
+    const [contactName, setContactName] = useState("");
+    const [contactEmail, setContactEmail] = useState("");
+    const [contactPhone, setContactPhone] = useState("");
+    const [contactMessage, setContactMessage] = useState("");
+
+    const [message, setMessage] = useState("");
+
+    async function handleContact(e) {
+        e.preventDefault();
+
+        const response = await fetch("http://localhost:3001/api/contact", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include",
+            body: JSON.stringify({
+                fullName: contactName,
+                email: contactEmail,
+                phone: contactPhone,
+                message: contactMessage
+            }),
+        });
+
+        const data = await response.json();
+        setMessage(data.message);
+    
+    }
+
     return (
         <div class="contactContainer">
             <div class="detailsCard">
@@ -12,20 +42,33 @@ export default function ContactForm() {
             </div>
 
             <div class="formCard">
-                <form id="contactForm">
+                <form id="contactForm" onSubmit={handleContact} className="stack">
                     <div class="formGrid">
                         <label for="name">Full Name:</label>
-                        <input type="text" id="name"></input>
+                        <input type="text"
+                            id="name"
+                            value={contactName}
+                            onChange={(e) => setContactName(e.target.value)}></input>
 
                         <label for="email">Email:</label>
-                        <input type="text" id="email" placeholder="example@gmail.com"
-                            pattern="[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}"></input>
+                        <input type="text"
+                            id="email"
+                            placeholder="example@gmail.com"
+                            pattern="[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}"
+                            value={contactEmail}
+                            onChange={(e) => setContactEmail(e.target.value)}></input>
 
                         <label for="phone">Phone Number:</label>
-                        <input type="tel" id="phone" placeholder="(123) 456-7890"></input>
+                        <input type="tel"
+                            id="phone"
+                            placeholder="(123) 456-7890"
+                            value={contactPhone}
+                            onChange={(e) => setContactPhone(e.target.value)}></input>
 
                         <label for="message">Message:</label>
-                        <textarea></textarea>
+                        <textarea
+                            value={contactMessage}
+                            onChange={(e) => setContactMessage(e.target.value)}></textarea>
                     </div>
 
                     <div class="contactButtons">
@@ -33,6 +76,9 @@ export default function ContactForm() {
                         <button id="contactReset" type="reset">Reset</button>
                     </div>
                 </form>
+
+                <p className="message">{message}</p>
+
             </div>
         </div>
     )
